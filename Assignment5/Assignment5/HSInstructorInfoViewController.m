ec141fb90593af69602052c7b828a1abebadcfee
Harsh <shahmharsh@gmail.com>
2014-11-15 08:47:46 -0800

    Added segue navigation
    - passed instructor object to InstrunctorInfoViewController through segue

--- Assignment5/Assignment5/HSInstructorInfoViewController.m
+++ Assignment5/Assignment5/HSInstructorInfoViewController.m
@@ -13,20 +13,20 @@
 @end
 
 @implementation HSInstructorInfoViewController
+@synthesize instructor = _instructor;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.title = [_instructor fullName];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)getInstructorData {
+    
 }
 
 @end
